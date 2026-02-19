f7c5c583cad820c545ce056cca07522ef0358193
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-02-19 14:31:09 -0800

    comment change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 
-// start server
+// start listening to server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
