d7660f1848493477aec4959f34d5443a92c8ac6a
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-02-19 12:33:22 -0800

    App.js created

---
+++ app.js
@@ -0,0 +1,21 @@
+// import express
+import express from 'express';
+
+// create app instance
+const app = express();
+
+// default route port
+const PORT = 3003;
+
+//enable static file serving
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
