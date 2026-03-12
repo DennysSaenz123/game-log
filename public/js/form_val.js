4e8ac17eb1c6f606077c0717d41711b4d93d62f8
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-03-12 15:23:50 -0700

    sign in form action

--- public/js/form_val.js
+++ public/js/form_val.js
@@ -50,43 +50,43 @@ document.getElementById("addGameForm").onsubmit = (event) => {
 
 };
 
-// validate register form
-document.getElementById("registerForm").onsubmit = (event) => {
-    clearErrors();
-    let isValid = true;
+// // validate register form
+// document.getElementById("registerForm").onsubmit = (event) => {
+//     clearErrors();
+//     let isValid = true;
 
-    let firstName = document.getElementById("firstName").value.trim();
-    if (!firstName) {
-        document.getElementById("err-firstName").style.display = "block";
-        isValid = false;
-    }
+//     let firstName = document.getElementById("firstName").value.trim();
+//     if (!firstName) {
+//         document.getElementById("err-firstName").style.display = "block";
+//         isValid = false;
+//     }
 
-    let lastName = document.getElementById("lastName").value.trim();
-    if (!lastName) {
-        document.getElementById("err-lastName").style.display = "block";
-        isValid = false;
-    }
+//     let lastName = document.getElementById("lastName").value.trim();
+//     if (!lastName) {
+//         document.getElementById("err-lastName").style.display = "block";
+//         isValid = false;
+//     }
 
-    let email = document.getElementById("email").value.trim();
-    if (!email) {
-        document.getElementById("err-email").style.display = "block";
-        isValid = false;
-    }
+//     let email = document.getElementById("email").value.trim();
+//     if (!email) {
+//         document.getElementById("err-email").style.display = "block";
+//         isValid = false;
+//     }
 
-    let username = document.getElementById("username").value.trim();
-    if (!username) {
-        document.getElementById("err-username").style.display = "block";
-        isValid = false;
-    }
+//     let username = document.getElementById("username").value.trim();
+//     if (!username) {
+//         document.getElementById("err-username").style.display = "block";
+//         isValid = false;
+//     }
 
-    let password = document.getElementById("password").value.trim();
-    if (!password) {
-        document.getElementById("err-password").style.display = "block";
-        isValid = false;
-    }
+//     let password = document.getElementById("password").value.trim();
+//     if (!password) {
+//         document.getElementById("err-password").style.display = "block";
+//         isValid = false;
+//     }
 
-    if (!isValid) event.preventDefault();
-};
+//     if (!isValid) event.preventDefault();
+// };
 
 
 // validate sign in form
